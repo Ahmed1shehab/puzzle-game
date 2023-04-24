ed33eb4946fe331de040eac71a20060aa42d1d19
Ahmed1shehab <116594194+Ahmed1shehab@users.noreply.github.com>
2023-04-25 00:07:08 +0200

    Add files via upload

---
+++ puzzlegame/puzzlegame/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace puzzlegame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gpPuzzleBox = new System.Windows.Forms.GroupBox();
+            this.lblMovesMade = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gpOriginal = new System.Windows.Forms.GroupBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.gpPuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpPuzzleBox
+            // 
+            this.gpPuzzleBox.Controls.Add(this.pbx1);
+            this.gpPuzzleBox.Controls.Add(this.pbx2);
+            this.gpPuzzleBox.Controls.Add(this.pbx3);
+            this.gpPuzzleBox.Controls.Add(this.pbx4);
+            this.gpPuzzleBox.Controls.Add(this.pbx5);
+            this.gpPuzzleBox.Controls.Add(this.pbx6);
+            this.gpPuzzleBox.Controls.Add(this.pbx7);
+            this.gpPuzzleBox.Controls.Add(this.pbx8);
+            this.gpPuzzleBox.Controls.Add(this.pbx9);
+            this.gpPuzzleBox.Location = new System.Drawing.Point(33, 4);
+            this.gpPuzzleBox.Name = "gpPuzzleBox";
+            this.gpPuzzleBox.Size = new System.Drawing.Size(427, 433);
+            this.gpPuzzleBox.TabIndex = 0;
+            this.gpPuzzleBox.TabStop = false;
+            this.gpPuzzleBox.Text = "PuzzleBox";
+            // 
+            // lblMovesMade
+            // 
+            this.lblMovesMade.AutoSize = true;
+            this.lblMovesMade.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovesMade.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblMovesMade.Location = new System.Drawing.Point(33, 458);
+            this.lblMovesMade.Name = "lblMovesMade";
+            this.lblMovesMade.Size = new System.Drawing.Size(217, 37);
+            this.lblMovesMade.TabIndex = 9;
+            this.lblMovesMade.Text = "Moves Made:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Impact", 48F);
+            this.lblTime.Location = new System.Drawing.Point(525, 380);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(264, 80);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // tmrTimer
+            // 
+            this.tmrTimer.Enabled = true;
+            this.tmrTimer.Interval = 900;
+            this.tmrTimer.Tick += new System.EventHandler(this.UpdateTimeElapsed);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button3.Image = global::puzzlegame.Properties.Resources.Quit;
+            this.button3.Location = new System.Drawing.Point(872, 479);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 81);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button2.Image = global::puzzlegame.Properties.Resources.ic_pause;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(688, 479);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 81);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.PauseOrResume);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Image = global::puzzlegame.Properties.Resources._05_shuffle__1_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(523, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 81);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Shuffle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gpOriginal
+            // 
+            this.gpOriginal.BackColor = System.Drawing.SystemColors.Control;
+            this.gpOriginal.BackgroundImage = global::puzzlegame.Properties.Resources.buggs;
+            this.gpOriginal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gpOriginal.Location = new System.Drawing.Point(523, 47);
+            this.gpOriginal.Name = "gpOriginal";
+            this.gpOriginal.Size = new System.Drawing.Size(603, 330);
+            this.gpOriginal.TabIndex = 10;
+            this.gpOriginal.TabStop = false;
+            this.gpOriginal.Text = " ";
+            // 
+            // pbx7
+            // 
+            this.pbx7.Location = new System.Drawing.Point(38, 248);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(137, 110);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 1;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx6
+            // 
+            this.pbx6.Location = new System.Drawing.Point(324, 132);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(137, 110);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 8;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx9
+            // 
+            this.pbx9.Location = new System.Drawing.Point(324, 248);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(137, 110);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 7;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx3
+            // 
+            this.pbx3.Location = new System.Drawing.Point(324, 19);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(137, 107);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 6;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(181, 19);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(137, 107);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 5;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx1
+            // 
+            this.pbx1.Location = new System.Drawing.Point(38, 19);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(137, 110);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 4;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx5
+            // 
+            this.pbx5.Location = new System.Drawing.Point(181, 132);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(137, 110);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 3;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx4
+            // 
+            this.pbx4.Location = new System.Drawing.Point(38, 132);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(137, 110);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 2;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx8
+            // 
+            this.pbx8.Location = new System.Drawing.Point(181, 248);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(137, 110);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 637);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.gpOriginal);
+            this.Controls.Add(this.lblMovesMade);
+            this.Controls.Add(this.gpPuzzleBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Puzzle-Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gpPuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpPuzzleBox;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.Label lblMovesMade;
+        private System.Windows.Forms.GroupBox gpOriginal;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer tmrTimer;
+    }
+}
+
---
+++ puzzlegame/puzzlegame/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzlegame
+{
+    public partial class Form1 : Form
+    {
+        int inNullSliceIndex, inmoves = 0;
+        List<Bitmap> lstOriginalPictureList = new List<Bitmap>();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1, Properties.Resources._2, Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7, Properties.Resources._8, Properties.Resources._9, Properties.Resources._null });
+            lblMovesMade.Text += inmoves;
+            lblTime.Text = "00:00:00";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+        void Shuffle()
+        {
+            do
+            {
+                int j;
+                List<int> Indexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });//8 is not present - since it is the last slice.
+                Random r = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    Indexes.Remove((j = Indexes[r.Next(0, Indexes.Count)]));
+                    ((PictureBox)gpPuzzleBox.Controls[i]).Image = lstOriginalPictureList[j];
+                    if (j == 9) inNullSliceIndex = i;//store empty picture box index
+                }
+            } while (CheckWin());
+        }
+
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult YesOrNo = new DialogResult();
+            if (lblTime.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("Are You Sure To RESTART ?", "Rabbit Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || lblTime.Text == "00:00:00")
+            {
+                Shuffle();
+                timer.Reset();
+                lblTime.Text = "00:00:00";
+                inmoves = 0;
+                lblMovesMade.Text = "Moves Made : 0";
+            }
+        }
+
+        private void AskPermissionBeforeQuite(object sender, FormClosingEventArgs e)
+        {
+            DialogResult YesOrNO = MessageBox.Show("Are You Sure To Quit ?", "Rabbit Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sender as Button != button3 && YesOrNO == DialogResult.No) e.Cancel = true;
+            if (sender as Button == button3 && YesOrNO == DialogResult.Yes) Environment.Exit(0);
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            AskPermissionBeforeQuite(sender, e as FormClosingEventArgs);
+        }
+
+        private void SwitchPictureBox(object sender, EventArgs e)
+        {
+            if (lblTime.Text == "00:00:00")
+                timer.Start();
+            int inPictureBoxIndex = gpPuzzleBox.Controls.IndexOf(sender as Control);
+            if (inNullSliceIndex != inPictureBoxIndex)
+            {
+                List<int> FourBrothers = new List<int>(new int[] { ((inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1), inPictureBoxIndex - 3, (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1, inPictureBoxIndex + 3 });
+                if (FourBrothers.Contains(inNullSliceIndex))
+                {
+                    ((PictureBox)gpPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gpPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                    ((PictureBox)gpPuzzleBox.Controls[inPictureBoxIndex]).Image = lstOriginalPictureList[9];
+                    inNullSliceIndex = inPictureBoxIndex;
+                    lblMovesMade.Text = "Moves Made : " + (++inmoves);
+                    if (CheckWin())
+                    {
+                        timer.Stop();
+                        (gpPuzzleBox.Controls[8] as PictureBox).Image = lstOriginalPictureList[8];
+                        MessageBox.Show("Congratulations...\nYour Rabbit Is Happy\nTime Elapsed : " + timer.Elapsed.ToString().Remove(8) + "\nTotal Moves Made : " + inmoves, "Rabbit Puzzle");
+                        inmoves = 0;
+                        lblMovesMade.Text = "Moves Made : 0";
+                        lblTime.Text = "00:00:00";
+                        timer.Reset();
+                        Shuffle();
+                    }
+                }
+            }
+        }
+
+        private void UpdateTimeElapsed(object sender, EventArgs e)
+        {
+            if (timer.Elapsed.ToString() != "00:00:00")
+                lblTime.Text = timer.Elapsed.ToString().Remove(8);
+            if (timer.Elapsed.ToString() == "00:00:00")
+                button2.Enabled = false;
+            else
+                button2.Enabled = true;
+            if (timer.Elapsed.Minutes.ToString() == "1")
+            {
+                timer.Reset();
+                lblMovesMade.Text = "Moves Made : 0";
+                lblTime.Text = "00:00:00";
+                inmoves = 0;
+                button2.Enabled = false;
+                MessageBox.Show("Time Is Up\nTry Again", "Rabbit Puzzle");
+                Shuffle();
+            }
+        }
+
+        bool CheckWin()
+        {
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                if ((gpPuzzleBox.Controls[i] as PictureBox).Image != lstOriginalPictureList[i]) break;
+            }
+            if (i == 8) return true;
+            else return false;
+        }
+        private void PauseOrResume(object sender, EventArgs e)
+        {
+            if (button2.Text == "Pause")
+            {
+                timer.Stop();
+                gpPuzzleBox.Visible = false;
+                button2.Text = "Resume";
+            }
+            else
+            {
+                timer.Start();
+                gpPuzzleBox.Visible = true;
+                button2.Text = "Pause";
+            }
+        }
+    }
+}
